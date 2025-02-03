f2c782e657f6c5e0f1d5ebcc984804efbb37047e
Aurora-Nasa-1 <B04031010@outlook.com>
2025-02-03 09:24:57 +0800

    feat: 更新 User.sh，隐藏压缩过程中的输出信息以减少控制台干扰

--- settings/script/User.sh
+++ settings/script/User.sh
@@ -78,7 +78,7 @@ fi
 print_KEY_title "$USER_PACK_MODULE_ZSTD" "$USER_PACK_MODULE"
 if [ "$key_pressed" = "KEY_VOLUMEUP" ]; then
     echo "- $USER_START_COMPRESS"
-    tar -cf "$NOW_PATH/output.tar" -C "$NOW_PATH" files/
+    tar -cf "$NOW_PATH/output.tar" -C "$NOW_PATH" files/ >/dev/null 2>&1
     zip_if "$?" "output.tar"
     $zstd -19 "$NOW_PATH/output.tar.zst" "$NOW_PATH/output.tar" >/dev/null 2>&1
     zip_if "$?" "output.tar.zst"
