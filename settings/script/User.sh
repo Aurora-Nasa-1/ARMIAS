7b89bef0210c530e7854e8e4cfef93c457080f5a
Aurora-Nasa-1 <B04031010@outlook.com>
2025-02-03 09:08:01 +0800

    feat: 更新 User.sh，修改路径变量以提高可读性并优化文件处理逻辑

--- settings/script/User.sh
+++ settings/script/User.sh
@@ -5,6 +5,8 @@
 # shellcheck disable=SC2155
 # shellcheck disable=SC2046
 # shellcheck disable=SC3045
+# shellcheck disable=SC2164
+NOW_PATH="/data/local/tmp"
 abort() {
     echo "$1"
     exit 1
@@ -19,8 +21,8 @@ print_KEY_title() {
     key_select
 }
 zip_if() {
-    if [ -f "$1" ]; then
-        return_code="$1"
+    if [ -z "$1" ]; then
+        return_code=$1
         if [ "$return_code" -eq 0 ]; then
             echo "- ${USER_SUCCESSFULLY_COMPRESS}: $2"
         else
@@ -32,29 +34,29 @@ zip_if() {
 main() {
     MODPATH=$(cat /data/local/tmp/clickinformation.txt)
     rm -rf "/data/local/tmp/clickinformation.txt"
-    if [ ! -f "/data/local/tmp/settings/settings.sh" ]; then
+    if [ ! -f "$NOW_PATH/settings/settings.sh" ]; then
         abort "Notfound File!!!(settings.sh)"
     else
         # shellcheck source=/dev/null
-        . "/data/local/tmp/settings/settings.sh"
+        . "$NOW_PATH/settings/settings.sh"
     fi
-    if [ ! -f "/data/local/tmp/$langpath" ]; then
+    if [ ! -f "$NOW_PATH/$langpath" ]; then
         abort "Notfound File!!!($langpath)"
     else
         # shellcheck disable=SC1090
-        . "/data/local/tmp/$langpath"
+        . "$NOW_PATH/$langpath"
         eval "lang_$print_languages"
     fi
-    if [ ! -f "/data/local/tmp/$script_path" ]; then
+    if [ ! -f "$NOW_PATH/$script_path" ]; then
         abort "Notfound File!!!($script_path)"
     else
         # shellcheck disable=SC1090
-        . "/data/local/tmp/$script_path"
+        . "$NOW_PATH/$script_path"
     fi
-    if [ -f "/data/local/tmp/prebuilts.tar.xz" ]; then
-        tar -xJf "/data/local/tmp/prebuilts.tar.xz" -C "/data/local/tmp/"
-        zstd="/data/local/tmp/prebuilts/zstd"
-        zips="/data/local/tmp/prebuilts/7zzs"
+    if [ -f "$NOW_PATH/prebuilts.tar.xz" ]; then
+        tar -xJf "$NOW_PATH/prebuilts.tar.xz" -C "$NOW_PATH/"
+        zstd="$NOW_PATH/prebuilts/zstd"
+        zips="$NOW_PATH/prebuilts/7zzs"
         chmod 755 "$zips"
         chmod 755 "$zstd"
     fi
@@ -62,41 +64,43 @@ main() {
 main
 print_KEY_title "$USER_KEY_BACKUPMODULE" "$USER_NOT_BACKUPMODULE"
 if [ "$key_pressed" = "KEY_VOLUMEUP" ]; then
-    print_KEY_title "$USER_KEY_BACKUPMODULE(Zip)" "${USER_KEY_BACKUPMODULE}Plus ($USER_CHOOSE_ZSTD)"
+    print_KEY_title "$USER_KEY_BACKUPMODULE(Zip)" "${USER_KEY_BACKUPMODULE}Zstd ($USER_CHOOSE_ZSTD)"
     if [ "$key_pressed" = "KEY_VOLUMEUP" ]; then
         echo "- $USER_START_ZIP"
         for DIR in "/data/adb/modules/"*/; do
             DIR_NAME=$(basename "$DIR")
-            OUTPUT_FILE="$MODPATH/files/modules/${DIR_NAME}.zip"
+            OUTPUT_FILE="$NOW_PATH/files/modules/${DIR_NAME}.zip"
             $zips a -r "$OUTPUT_FILE" "$DIR*" >/dev/null 2>&1
         done
-        echo "- $USER_ZIPPED $MODPATH/files/modules"
+        echo "- $USER_ZIPPED $NOW_PATH/files/modules"
     else
         echo "- $USER_START_COPY_FILE"
-        cp -r "/data/adb/modules"/* "$MODPATH/files/modules" >/dev/null 2>&1
-        echo "- $USER_END_COPY_FILE $MODPATH/files/modules"
+        cp -r "/data/adb/modules"/* "$NOW_PATH/files/modules" >/dev/null 2>&1
+        echo "- $USER_END_COPY_FILE $NOW_PATH/files/modules"
     fi
 fi
 print_KEY_title "$USER_PACK_MODULE_ZSTD" "$USER_PACK_MODULE"
 if [ "$key_pressed" = "KEY_VOLUMEUP" ]; then
     echo "- $USER_START_COMPRESS"
-    tar -cf "$MODPATH/output.tar" -C "$MODPATH" files/
+    tar -cf "$NOW_PATH/output.tar" -C "$NOW_PATH" files/
     zip_if "$?" "output.tar"
-    $zstd -ultra -22 "$MODPATH/output.tar.zst" "$MODPATH/output.tar" >/dev/null 2>&1
+    $zstd -19 "$NOW_PATH/output.tar.zst" "$NOW_PATH/output.tar" >/dev/null 2>&1
     zip_if "$?" "output.tar.zst"
-    rm "$MODPATH"/output.tar
-    $zips a "$MODPATH"/ARMIAS.zip "$MODPATH/"* -xr!"$MODPATH/files/*" >"/dev/null" 2>&1
+    rm "$NOW_PATH"/output.tar
+    $zips a -r "$MODPATH"/ARMIAS.zip "$NOW_PATH/"* -xr!"$NOW_PATH/files/" >"/dev/null" 2>&1
     zip_if "$?" "ARMIAS.zip"
 else
     echo "- $USER_START_COMPRESS"
-    $zips a "$MODPATH"/ARMIAS.zip "$MODPATH/"* >"/dev/null" 2>&1
+    $zips a -r "$MODPATH"/ARMIAS.zip "$NOW_PATH/"* >"/dev/null" 2>&1
     zip_if "$?" "ARMIAS.zip"
 fi
-print_KEY_title "清理剩余文件并退出(! 谨慎)" "退出"
+print_KEY_title "$USER_CLEAN_REMAINING_EXIT" "$EXIT"
 if [ "$key_pressed" = "KEY_VOLUMEUP" ]; then
     find "$MODPATH" -mindepth 1 ! -name "ARMIAS.zip" -exec rm -rf {} +
 fi
-rm -rf /data/local/tmp/settings/
-rm -rf /data/local/tmp/prebuilts.tar.xz
-rm -rf /data/local/tmp/prebuilts/
-abort "- Done"
+rm -rf $NOW_PATH/files/
+rm -rf $NOW_PATH/settings/
+rm -rf $NOW_PATH/prebuilts.tar.xz
+rm -rf $NOW_PATH/prebuilts/
+echo "- Done"
+exit 0
