105df86b5a3b3912a726fb0fb573411e45c0624d
Aurora-Nasa-1 <b04031010@outlook.com>
2025-02-07 15:40:22 +0800

    fix: 从 Click.sh 中移除对版本信息的传递以简化脚本调用

--- Click.sh
+++ Click.sh
@@ -43,9 +43,10 @@ detect_environment() {
 detect_environment
 MODPATH=${0%/*}
 VERSION=$(grep "version" "$MODPATH/module.prop" | awk -F'=' '{print $2}')
+echo "Module Version: $VERSION"
 NOW_PATH="/data/local/tmp"
 cp -r "$MODPATH"/files/ "$NOW_PATH"/
 cp -r "$MODPATH"/settings/ "$NOW_PATH"/
 cp "$MODPATH"/prebuilts.tar.xz "$NOW_PATH"/
 chmod -R 755 "$NOW_PATH"/settings/
-ASH_STANDALONE=1 $BUSYBOX_PATH sh "$NOW_PATH"/settings/script/User.sh "$MODPATH" "$NOW_PATH" "$VERSION"
+ASH_STANDALONE=1 $BUSYBOX_PATH sh "$NOW_PATH"/settings/script/User.sh "$MODPATH" "$NOW_PATH"
